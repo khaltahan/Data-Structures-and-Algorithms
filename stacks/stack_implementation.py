757588d6a4ff8e66629f3e5b90b723c8831eb49c
Khalid <khaltahan@gmail.com>
2024-01-22 15:15:11 -0500

    stack push method

--- stacks/stack_implementation.py
+++ stacks/stack_implementation.py
@@ -4,3 +4,13 @@ class Stack:
         self.arr = [0 for _ in range(initial_size)]
         self.next_index = 0
         self.num_elements = 0
+
+    def push (self, data):
+        self.arr[self.next_index] = data
+        self.next_index += 1
+        self.num_elements += 1
+
+foo = Stack()
+foo.push("Test!")
+print(foo.arr)
+print("Pass" if foo.arr[0] == "Test!" else "Fail")
